df02ef5c285cd3d4386909de68a70345305a9fcf
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2023-10-03 11:19:04 +0700

    config: tailwind preparation package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  tailwindcss: {
+    // viewer: false,
+    exposeConfig: true,
+    injectPosition: 'last',
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config'
+  },
 })
