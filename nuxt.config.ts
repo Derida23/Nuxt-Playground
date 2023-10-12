70c4d366b0bc01f455f6b0c366d1ff330e2aa053
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2023-10-12 12:53:22 +0700

    config: nuxt ui jakal boilerlate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,30 @@
+import { CSRF_CONFIG, SECURITY_CONFIG } from './constants/security'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-security',
+    '@nuxtjs/google-fonts',
+  ],
+  pinia: {
+    autoImports: ['defineStore', 'storeToRefs'],
+  },
+  googleFonts: {
+    preload: true,
+    families: {
+      Inter: [400, 500, 600, 700],
+    },
+  },
+  csurf: CSRF_CONFIG,
+  security: SECURITY_CONFIG,
+  colorMode: {
+    preference: 'light', // disable dark mode
+  },
+  imports: {
+    dirs: ['stores', 'composables/api'],
+  },
 })
